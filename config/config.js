c88b82c171892029c9a7caeeaa6e9f1856ed7955
Shreyash Gedam <shreyashakgedam@gmail.com>
2023-04-21 20:33:53 +0530

    config changes

--- config/config.js
+++ config/config.js
@@ -1,5 +1,6 @@
 const { default: mongoose } = require("mongoose");
 
 const connection = mongoose.connect("mongodb://localhost:27017/InsuredMine")
+// const connection = mongoose.connect("mongodb://127.0.0.1:27017/InsuredMine")
 
 module.exports = connection
